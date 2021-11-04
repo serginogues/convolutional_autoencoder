3dd78714b7d7f201f531aca2639ae1d43225060e
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-11-04 12:12:19 +0100

    test git

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# This is a sample Python script.
+"""
+ACML Assignment 2: Convolutional Autoencoder (CAE)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+CAE features:
+    - The output is evaluated by comparing the reconstructed image by the original one
+    - CIFAR-10 dataset: https://www.cs.toronto.edu/~kriz/cifar.html
+
+Tasks:
+1) Reconstruction with Deep Learning library  (PyTorch)
+    - Goal: Reconstruct any CIFAR-10 image
+    - Cost function: MSE or binary cross-entropy between input and reconstructed image
+    - Activation for conv layers: ReLU
+    - Architecture:
+        Input: 32x32x3 (colored image)
+        Encoder:
+            Layer 1: Filter: 3x3 and 8 channels -> Max pooling 2x2
+            Layer 2: Filter: 3x3 + Channels: 12 -> Max pooling 2x2
+            Layer 3: 
+
+
+"""
 
 
 def print_hi(name):
