f0a089740d0294128f65e611ae779b5045468372
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-11-07 19:13:08 +0100

    colorization

--- main.py
+++ main.py
@@ -7,89 +7,131 @@ from autoencoder import *
 import cv2
 
 if __name__ == '__main__':
-    images, _ = iter(test_loader).next()
-    images_plot = images.numpy()
-
-
-    class CAE_colorization(nn.Module):
+    class CAE4(nn.Module):
         def __init__(self):
-            super(CAE_colorization, self).__init__()
+            super(CAE4, self).__init__()
 
-            channels = [8, 12, 16, 20, 16]
+            channels = [8, 16, 32, 64, 128, 32, 8]
+            padding = 1
+            stride = 1
+            kernel = 3
 
             # Encoder
-            self.conv1 = nn.Conv2d(in_channels=1, out_channels=channels[0], kernel_size=(3, 3), padding=1, stride=1)
-            self.conv2 = nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=(3, 3), padding=1,
-                                   stride=1)
-            self.conv3 = nn.Conv2d(in_channels=channels[1], out_channels=channels[2], kernel_size=(3, 3), padding=1,
-                                   stride=1)
-            self.conv4 = nn.Conv2d(in_channels=channels[2], out_channels=channels[3], kernel_size=(3, 3), padding=1,
-                                   stride=1)
-            self.maxpool = nn.MaxPool2d(kernel_size=(2, 2), stride=1, padding=0)
+            self.conv1 = nn.Conv2d(in_channels=1, out_channels=channels[0], kernel_size=(3, 3), padding=padding,
+                                   stride=stride)
+            self.conv2 = nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=(3, 3),
+                                   padding=padding, stride=stride)
+            self.conv3 = nn.Conv2d(in_channels=channels[1], out_channels=channels[2], kernel_size=(3, 3),
+                                   padding=padding, stride=stride)
+            self.conv4 = nn.Conv2d(in_channels=channels[2], out_channels=channels[3], kernel_size=(3, 3),
+                                   padding=padding, stride=stride)
+            self.conv5 = nn.Conv2d(in_channels=channels[3], out_channels=channels[4], kernel_size=(3, 3),
+                                   padding=padding, stride=stride)
+            self.maxpool = nn.MaxPool2d(kernel_size=kernel - 1, stride=stride, padding=0)
 
             # Decoder
-            self.t_conv1 = nn.ConvTranspose2d(in_channels=channels[3], out_channels=channels[4], kernel_size=(4, 4),
-                                              padding=1, stride=1)
-            self.t_conv2 = nn.ConvTranspose2d(in_channels=channels[4], out_channels=channels[1], kernel_size=(4, 4),
-                                              padding=1, stride=1)
-            self.t_conv3 = nn.ConvTranspose2d(in_channels=channels[1], out_channels=channels[0], kernel_size=(3, 3),
-                                              padding=1, stride=1)
-            self.t_conv4 = nn.ConvTranspose2d(in_channels=channels[0], out_channels=2, kernel_size=(3, 3), padding=1,
-                                              stride=1)
+            self.t_conv1 = nn.ConvTranspose2d(in_channels=channels[4], out_channels=channels[5], kernel_size=(5, 5),
+                                              padding=padding, stride=stride)
+            self.t_conv2 = nn.ConvTranspose2d(in_channels=channels[5], out_channels=channels[6], kernel_size=(4, 4),
+                                              padding=padding, stride=stride)
+            self.t_conv3 = nn.ConvTranspose2d(in_channels=channels[6], out_channels=2, kernel_size=(4, 4),
+                                              padding=padding, stride=stride)
             self.print_latent_shape = 0
 
         def forward(self, x):
             # encoder
-            x1 = F.relu(self.conv1(x))
-
-            x2 = self.maxpool(x1)
-            x3 = F.relu(self.conv2(x2))
-            x4 = self.maxpool(x3)
-            x5 = F.relu(self.conv3(x4))
-            x5 = F.relu(self.conv4(x5))
-
+            x = F.relu(self.conv1(x))
+            x = self.maxpool(x)
+            x = F.relu(self.conv2(x))
+            x = self.maxpool(x)
+            x = F.relu(self.conv3(x))
+            x = self.maxpool(x)
+            x = F.relu(self.conv4(x))
+            x = self.maxpool(x)
+            x = F.relu(self.conv5(x))
             if self.print_latent_shape == 0:
-                print(str(x1.shape) + " -> ")
-                print(str(x2.shape) + " -> ")
-                print(str(x3.shape) + " -> ")
-                print(str(x4.shape) + " -> ")
-                print("Latent space shape = " + str(x5.shape))
+                print("Latent space shape: " + str(x.shape))
                 self.print_latent_shape += 1
 
             # decoder
-            x6 = F.relu(self.t_conv1(x5))
-            x6 = F.relu(self.t_conv2(x6))
-            x6 = F.relu(self.t_conv3(x6))
-            y = F.sigmoid(self.t_conv4(x6))
-
-            if self.print_latent_shape == 0:
-                print(str(x6.shape) + " -> " + str(y.shape))
-                self.print_latent_shape += 1
-
+            x = F.relu(self.t_conv1(x))
+            x = F.relu(self.t_conv2(x))
+            y = F.sigmoid(self.t_conv3(x))
             return y
 
-    # model = CAE_colorization(channels=[5, 12, 16, 12])
-    model_colorization = CAE_colorization()
-    model_colorization.load_state_dict(torch.load('models/cae3.pth'))
-    model.eval()
-    img = images[50].numpy()
-    img = np.transpose(img, (1, 2, 0))
-    yuv_image = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    model_colorization = CAE4().to(device)
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model_colorization.parameters(), lr=LR)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+
+    EPOCHS = 70
+
+    from torchvision import transforms
+    def train4(model, save):
+        loss_history = []
+        running_loss = 0.0
+        for i in range(EPOCHS):
+            # TRAIN MODEL
+            loss_sum = 0
+            n = 0
+            for j, data in enumerate(train_loader, 0):
+                n = j
+                # get the training data
+                imgs, _ = data
+
+                # rgb to yuv
+                luminance = torch.zeros((BATCH_SIZE, 1, 32, 32))
+                chrominance = torch.zeros((BATCH_SIZE, 2, 32, 32))
+                for idx in range(BATCH_SIZE):
+                    img = imgs[idx].numpy()
+                    yuv_image = cv2.cvtColor(np.transpose(img, (1, 2, 0)), cv2.COLOR_BGR2YUV)
+                    luminance[idx] = transforms.ToTensor()(yuv_image[:, :, 0])
+                    chrominance[idx] = transforms.ToTensor()(yuv_image[:, :, 1:])
+
+                luminance = luminance.float().to(device)
+                chrominance = chrominance.float().to(device)
+
+                # Before the backward pass, set gradients to zero
+                optimizer.zero_grad()
+
+                # predict
+                #input = images_yuv_tensor.float().to(device)
+                output = model.forward(luminance)  # chrominance
+                # compute loss
+                loss = criterion(output, chrominance)
+                loss_sum += round(float(loss.item()), 4)
+
+                # backpropagate loss error
+                loss.backward()
+
+                # optimize with backprop
+                optimizer.step()
+                del data, imgs
+
+            scheduler.step()
+
+            # region print current loss
+            loss_epoch = loss_sum / n
+            loss_history.append(loss_epoch)
+            print("Epoch " + str(i) + ", Loss = " + str(loss_epoch))
+
+            if i > 1 and loss_history[-1] < loss_history[-2]:
+                # SAVE THE MODEL
+                print("model saved")
+                torch.save(model.state_dict(), save)
+
+            if (i > 2 and loss_epoch > loss_history[-2]) or (i > 15 and (loss_history[1] - loss_epoch) < 0.02):
+                break
+
+        print("Training finished")
+
+        # PLOT ACCURACY
+        plt.plot(loss_history)
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.title('Training Loss per epoch')
+        plt.show()
+
+
     # %%
-    luminance_image = torch.tensor(np.reshape(yuv_image[:, :, 0], (1, 1, 32, 32)))
-    # chr_image = yuv_image[:,:,1:]
-    chr_image = model.forward(luminance_image)
-    chr_image = np.reshape(chr_image.detach().numpy(), (32, 32, 2))
-    yuv_reconstructed = cv2.merge((yuv_image[:, :, 0], chr_image[:, :, 0], chr_image[:, :, 1]))
-    # %%
-    reconstructed = cv2.cvtColor(yuv_reconstructed, cv2.COLOR_YUV2BGR)
-    plt.subplot(1, 3, 1)
-    plt.imshow(img)
-    plt.title('Original RGB')
-    plt.subplot(1, 3, 2)
-    plt.imshow(yuv_image[:, :, 0], cmap="gray")
-    plt.title('Original Gray')
-    plt.subplot(1, 3, 3)
-    plt.imshow(reconstructed)
-    plt.title('Colorized')
-    plt.show()
+    train4(model_colorization, " ")
